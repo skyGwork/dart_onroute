b534a6a3d5bb0329b5053575620323d129e43d84
skyGwork <skyGworkk@gmail.com>
2022-08-01 21:18:14 +0530

    create dart_onroute

---
+++ bin/dart_onroute.dart
@@ -0,0 +1,5 @@
+import 'package:dart_onroute/dart_onroute.dart' as dart_onroute;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_onroute.calculate()}!');
+}
---
+++ test/dart_onroute_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_onroute/dart_onroute.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
